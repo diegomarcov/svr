7b8b8cf430e8410a9aa35c849acf57f7d8227cbe
unknown <EMontenegro@.(none)>
2010-10-30 13:06:33 -0300

    asignacion de signals y creacion del main

---
+++ svr/main.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt4 import QtCore, QtGui
+from mainWindow import Ui_MainWindow
+
+class Principal (QtGui.QMainWindow):
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+        
+        self.ventana = Ui_MainWindow()
+        self.ventana.setupUi(self)
+        
+        self.connect(self.ventana.btnVuelos, QtCore.SIGNAL('clicked()'), self.mostrarTabVuelos)
+        self.connect(self.ventana.btnInstanciasVuelos, QtCore.SIGNAL('clicked()'), self.mostrarTabInstVuelos)
+        
+    def mostrarTabVuelos(self):
+        self.ventana.stackedWidget.setCurrentIndex(1)
+    
+    def mostrarTabInstVuelos(self):
+        self.ventana.stackedWidget.setCurrentIndex(0)
+        
+def main ():
+        app = QtGui.QApplication (sys.argv)
+        ventana = Principal()
+        ventana.show()
+        sys.exit(app.exec_())
+        
+if __name__ == '__main__':
+    main()
+    
