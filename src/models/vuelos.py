1202afdf40ed345427dc2b660dc6e6c10925b6fa
Diego <diegomarcov@gmail.com>
2010-11-13 17:43:29 -0300

    Agregada el alta y modificacion de vuelos en la capa de abstraccion de la BD.

--- src/models/vuelos.py
+++ src/models/vuelos.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from abstractmodel import AbstractModel
 
 from PyQt4 import QtCore
@@ -8,3 +9,19 @@ class Vuelos(AbstractModel):
 
         self.tableName = "vuelos"
         self.id = "id"
+
+        
+# CREATE TABLE vuelos (
+    # id                  VARCHAR(30) NOT NULL,
+    # aeropuerto_salida   VARCHAR(30) NOT NULL,
+    # aeropuerto_llegada  VARCHAR(30) NOT NULL,
+    # FOREIGN KEY (aeropuerto_salida)  REFERENCES aeropuertos (codigo),
+    # FOREIGN KEY (aeropuerto_llegada) REFERENCES aeropuertos (codigo),
+    # PRIMARY KEY (id)
+# ) ENGINE=InnoDB; 
+        
+    def insert(self, id, salida, llegada):
+        self.conn.update("insert into "+self.tableName+" (id, aeropuerto_salida, aeropuerto_llegada) values ('"+id+"', '"+salida+"', '"+llegada+"')")
+    
+    def update(self, oldid, newid, salida, llegada):
+        self.conn.update("update "+self.tableName+" set id='"+newid+"', aeropuerto_salida='"+salida+"', aeropuerto_llegada='"+llegada+"' where id='"+oldid+"'")
